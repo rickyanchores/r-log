eab8ccf7038dffc9c10412485f38c7095b6a8b00
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-07 12:23:54 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./styles.scss";
+import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Pages/Home/Home";
