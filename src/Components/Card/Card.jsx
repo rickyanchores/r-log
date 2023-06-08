d436d68932d9cd1275b45f895b5a4dd14020948b
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-08 18:21:38 +0100

    update

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='Card'>
+        <h3>Card</h3>
+        <p>This Card</p>
+    </div>
+  )
+}
+
+export default Card;
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "../../Components/Nav/Card/Card";
 
 const Gallery = () => {
   return (
     <div className="Gallery">
       <h3>Gallery</h3>
       <p>This is gallery</p>
+      <Card />
     </div>
   );
 };
